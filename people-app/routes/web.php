c45ecc3d9ecddaee3a81a65087b7759eb8b604f8
unknown <arkadiusz.ostrowski@student.wsb.edu.pl>
2023-05-23 21:30:59 +0200

    Added routes

--- people-app/routes/web.php
+++ people-app/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UniqueController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/ostrowski/50400/people', [UniqueController::class, 'index']);
+Route::post('/ostrowski/50400/people', [UniqueController::class, 'create']);
+Route::get('/ostrowski/50400/people/{id}', [UniqueController::class, 'show']);
+Route::put('/ostrowski/50400/people/{id}', [UniqueController::class, 'update']);
+Route::delete('/ostrowski/50400/people/{id}', [UniqueController::class, 'destroy']);
+Route::get('/token', function () {
+    return csrf_token();
 });
