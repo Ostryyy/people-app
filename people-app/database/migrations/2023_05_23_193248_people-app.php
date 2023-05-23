54ba5a383e37eba27e2b86af486b2ad2415ba1c3
unknown <arkadiusz.ostrowski@student.wsb.edu.pl>
2023-05-23 21:35:33 +0200

    prepared migration

--- people-app/database/migrations/2023_05_23_193248_people-app.php
+++ people-app/database/migrations/2023_05_23_193248_people-app.php
@@ -3,22 +3,40 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Faker\Factory as Faker;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+return new class extends Migration {
+    public function up()
     {
-        //
+        Schema::create('people-app', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone_number');
+            $table->string('street');
+            $table->string('city');
+            $table->string('country');
+            $table->timestamps();
+        });
+
+        $faker = Faker::create();
+
+        for ($i = 0; $i < 200; $i++) {
+            DB::table('people-app')->insert([
+                'first_name' => $faker->firstName,
+                'last_name' => $faker->lastName,
+                'phone_number' => $faker->phoneNumber,
+                'street' => $faker->streetAddress,
+                'city' => $faker->city,
+                'country' => $faker->country,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
-        //
+        Schema::dropIfExists('people-app');
     }
 };
